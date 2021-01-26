be641da944ce002b3e8e49ef237e2e06bd2aa170
Adel1818 <adelelechi9@gmail.com>
2021-01-26 23:58:28 +0100

    fix delete client

--- src/Pages/Client/List.js
+++ src/Pages/Client/List.js
@@ -3,12 +3,18 @@ import Table from 'react-bootstrap/Table'
 import Container from 'react-bootstrap/Container'
 import Button from 'react-bootstrap/Button'
 import axios from 'axios';
+import { useHistory } from "react-router-dom";
+ 
 
-export default class Home extends React.Component {  
-    state = {
+export default  class Home extends React.Component{
+   constructor(){
+      super();
+      this.state = {
         clients: []
-    }
-
+      }
+      this.delete = this.delete.bind(this);
+   }
+   
     componentDidMount() {
         axios.get(process.env.REACT_APP_API_END_POINT_URI + `/api/clients`)
           .then(res => {
@@ -17,56 +23,83 @@ export default class Home extends React.Component {
             this.setState({ clients });
           })
     }
-
-    render() {       
-        return (
-            <Container style={{ marginTop: '100px' }}>
-                <h1>Liste des clients</h1>
-
-                <Button variant="success" style={{ float: 'right', margin: '20px' }} className="btn btn-secondary" 
-                onClick={() => this.props.history.push('/clients/create')}>Ajouter client</Button>
-                <Table striped bordered hover>
-                    <thead>
-                        <tr>
-                        <th>ID</th>
-                        <th>Nom & prenom</th>
-                        <th>Email</th>
-                        <th>GSM</th>
-                        <th>Ville</th>
-                        <th>Action</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-
-                        { this.state.clients.map(client => 
-                            <tr>
-                                <td>{client.idClient}</td>
-                                <td>{client.nomClient} {client.prenomClient}</td>
-                                <td>{client.emailClient}</td>
-                                <td>{client.gsmClient}</td>
-                                <td>{client.villeAdrClient}</td>
-                                <td style={{ width:'200px', margin:5 }}>
-                                    <Button onClick={() => this.props.history.push("/clients/" + client.idClient )} style={{margin:5}}> Update</Button> 
-                                    <Button variant="danger" style={{margin:5}}
-                                        onClick={() => { 
-                                                            if (window.confirm('Are you sure you wish to delete this item?') ) 
-                                                            {          
-                                                                axios.delete(process.env.REACT_APP_API_END_POINT_URI + `/api/clients/`+ client.idClient)   
-                                                
-                                                            }
- 
-                                                       } 
-                                                    }
-                                    >Delete</Button> 
-                                </td>
-                            </tr>
-                        )}
-
-                    </tbody>
-                </Table>
-            </Container>
-        )
-    }
+   
+   
+   
+   delete(id){
+      this.setState(prevState => ({
+          clients: prevState.clients.filter(el => el != id )
+      }));
+   }
+   
+   render(){
+      return(
+      
+        <Container style={{ marginTop: '100px' }}>
+            <h1>Liste des clients</h1>
+    
+            <Button variant="success" style={{ float: 'right', margin: '20px' }} className="btn btn-secondary" 
+            onClick={() => this.props.history.push('/clients/create')}>Ajouter client</Button>
+            <Table striped bordered hover>
+                <thead>
+                    <tr>
+                    <th>ID</th>
+                    <th>Nom & prenom</th>
+                    <th>Email</th>
+                    <th>GSM</th>
+                    <th>Ville</th>
+                    <th>Action</th>
+                    </tr>
+                </thead>
+                
+          
+                    <Child delete={this.delete} data={this.state.clients} {...this.props} />
+                  
+                  
+              </Table>
+          </Container>
+      );
+   }
 }
 
+class Child extends React.Component{
+
+
+
+   delete(client){       
+      
+        if (window.confirm('Are you sure you wish to delete this item?') ) 
+        {          
+            axios.delete(process.env.REACT_APP_API_END_POINT_URI + `/api/clients/`+ client.idClient)   
+            this.props.delete(client);
+        }
+   }
+   
+   render(){   
+      return(
+         <tbody>
+           {
+              this.props.data.map(client =>
+
+                  <tr>
+                      <td>{client.idClient}</td>
+                      <td>{client.nomClient} {client.prenomClient}</td>
+                      <td>{client.emailClient}</td>
+                      <td>{client.gsmClient}</td>
+                      <td>{client.villeAdrClient}</td>
+                      <td style={{ width:'200px', margin:5 }}>
+                          <Button onClick={() => this.props.history.push("/clients/" + client.idClient )} style={{margin:5}}> Update</Button> 
+                          <Button variant="danger" style={{margin:5}}
+                              onClick={this.delete.bind(this, client)}     
+                          >Delete</Button> 
+                      </td>
+                  </tr>
+ 
+              )
+ 
+           }
+         </tbody>
+      )
+   }
+}
  
